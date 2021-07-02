8c8d3a100a3c2829362c9a8a45ff65afd87f026a
Alejandro Ortiz <aleortbas@gmail.com>
2021-07-02 12:00:45 -0500

    React components part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {NavbarBrand, Navbar} from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <Navbar dark color="primary">
        <div className="Container">
          <NavbarBrand href="/"> Ristorante con fusion</NavbarBrand>
        </div>
      </Navbar>
+     <Menu />
     </div>
   );
 }
