c210c96aa602ffaa1f45edf1c2dec984f036d24c
Alejandro Ortiz <aleortbas@gmail.com>
2021-07-06 11:27:56 -0500

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import {NavbarBrand, Navbar} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+render(){
   return (
     <div>
      <Navbar dark color="primary">
@@ -12,9 +22,10 @@ function App() {
          <NavbarBrand href="/"> Ristorante con fusion</NavbarBrand>
        </div>
      </Navbar>
-     <Menu />
+     <Menu dishes={this.state.dishes} />
     </div>
   );
+}
 }
 
 export default App;
