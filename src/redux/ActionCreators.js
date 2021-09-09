ada4d4539c59bc37fbc0665161777e04f163ee3e
Alejandro Ortiz <aleortbas@gmail.com>
2021-09-09 16:32:00 -0500

    Fecth server sin terminar

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import {DISHES} from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl';
 
 
 export const addComment = (dishID, rating, author, comment) => ({
@@ -15,9 +16,9 @@ export const addComment = (dishID, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
-    setTimeout(() => {
-        dispatch(addDishes(DISHES));
-    }, 2000)
+    return fetch(baseUrl + 'dishes')
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)));
 }
 
 export const dishesLoading = () => ({
